641ef4ada2b9e66ea108508be375a54d8ad5d86d
Pinyk <1748708966@qq.com>
2019-01-29 13:09:42 +0800

    nice

--- java学习2/supreme.java
+++ java学习2/supreme.java
@@ -1,4 +1,13 @@
 package words;
-
+//类的两种构造方法
 public class supreme {
+    public supreme()
+    {
+        this("this 调用有参构造方式");
+        System.out.println("无参构造方法");
+    }
+    public supreme(String name)
+    {
+        System.out.println("有参构造方式");
+    }
 }
