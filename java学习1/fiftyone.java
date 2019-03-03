8947747ce6806dc49396d949421f94f6663147c8
Pinyk <1748708966@qq.com>
2019-03-03 13:49:10 +0800

    nice

---
+++ java学习1/fiftyone.java
@@ -0,0 +1,12 @@
+package ke;
+
+class fiftyone {
+    public static void draw(fiftyone q){
+    }
+}
+public class fiftyon extends fiftyone{
+    public static void main(String args[]){
+        fiftyon p=new fiftyon();
+        draw(p);
+    }
+}
