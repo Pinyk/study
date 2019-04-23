665fb8aaa2172c6dee74325dbe98a9ee7ac990a4
Pinyk <1748708966@qq.com>
2019-04-23 16:53:09 +0800

    nice

--- java进阶/Java新特性/src/com/two/Main.java
+++
@@ -1,9 +0,0 @@
-package com.two;
-import com.two.A;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(A.add(10,20));
-        System.out.println(A.div(10,20));
-    }
-}
